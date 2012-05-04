139c9da8b6f780b808a880818cfa6329d97f7808
Jeremy Grosser <jeremy@synack.me>
2012-05-04 00:18:02 -0700

    Support full NOAAPORT stream with NBS1 protocol

--- download.py
+++ download.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 import logging
 import socket
-import emwin
+import os.path
+import os
+
+import nbs
 
 
 class Streamer(object):
     def __init__(self, hosts):
         self.hosts = hosts
-        self.log = logging.getLogger('emwin')
+        self.log = logging.getLogger('nbs')
         self.log.setLevel(logging.DEBUG)
         self.files = {}
 
@@ -15,7 +18,7 @@ class Streamer(object):
         self.log.info('Connecting to %s:%i' % host)
         self.sock = socket.socket()
         self.sock.connect(host)
-        return emwin.Connection(self.sock)
+        return nbs.Connection(self.sock)
 
     def reliable_stream(self):
         i = 0
@@ -30,25 +33,39 @@ class Streamer(object):
     def handle_incoming(self, filename, content):
         del self.files[filename]
         self.log.debug(filename)
+        filename = '/'.join(filename.upper().rsplit('.', 1)[0].split('_', 1))
+        ccblen = 2 * (((ord(content[0]) & 63) << 8) + ord(content[1]))
+
+        path = os.path.dirname(os.path.join('data/', filename))
+        if not os.path.exists(path):
+            os.makedirs(path)
+        fd = file('data/%s' % filename, 'w')
+        fd.write(content[ccblen+2:])
+        fd.flush()
+        fd.close()
 
     def run(self):
         for packet in self.reliable_stream():
             if not packet.filename in self.files:
-                self.files[packet.filename] = emwin.FileAssembler(
+                self.files[packet.filename] = nbs.FileAssembler(
                     packet.filename, self.handle_incoming)
             assembler = self.files[packet.filename]
             assembler.add_part(packet)
+            #self.log.debug(repr(packet))
 
 
 def main():
     s = Streamer([
-        ('1.pool.iemwin.net', 2211),
-        ('2.pool.iemwin.net', 2211),
-        ('1.nbsp.inoaaport.net', 2211),
-        ('2.nbsp.inoaaport.net', 2211),
-        ('3.nbsp.inoaaport.net', 2211),
-        ('texoma.wxpro.net', 2211),
-        ('emwin.aprsfl.net', 2211),
+        ('1.nbsp.inoaaport.net', 2210),
+    #    ('2.nbsp.inoaaport.net', 2210),
+    #    ('3.nbsp.inoaaport.net', 2210),
+    #    ('1.pool.iemwin.net', 2211),
+    #    ('2.pool.iemwin.net', 2211),
+    #    ('1.nbsp.inoaaport.net', 2211),
+    #    ('2.nbsp.inoaaport.net', 2211),
+    #    ('3.nbsp.inoaaport.net', 2211),
+    #    ('texoma.wxpro.net', 2211),
+    #    ('emwin.aprsfl.net', 2211),
     ])
     s.run()
 
---
+++ nbs.py
@@ -0,0 +1,102 @@
+from time import strptime, mktime, time
+from collections import namedtuple
+import logging
+import struct
+import sys
+
+
+handler = logging.StreamHandler()
+handler.setFormatter(logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s'))
+log = logging.getLogger('nbs')
+log.addHandler(handler)
+log.setLevel(logging.DEBUG)
+
+
+NBSHeader = namedtuple('NBSHeader', 'seq product_type product_category product_code channel_index filename compressed num_blocks block_number block_size')
+
+
+class NBSPacket(object):
+    HEADER_FMT = '>IBBBB37s?HHI'
+
+    def __init__(self, data):
+        header_size = struct.calcsize(self.HEADER_FMT)
+        p = struct.unpack(self.HEADER_FMT, data[:header_size])
+        self.header = dict(NBSHeader(*p)._asdict())
+        self.filename = self.header['filename'].rstrip('\x00')
+
+        if len(data) - header_size != self.header['block_size']:
+            log.warning('Packet block size mismatch: expected: %i  got: %i' % (
+                self.header['block_size'], len(data) - header_size))
+        self.data = data[header_size:]
+
+    def checksum(self):
+        s = 0
+        for c in self.data:
+            s += ord(c)
+        return s % 4294967296 # uint32 max
+
+    def __str__(self):
+        return self.filename
+
+    def __repr__(self):
+        return 'NBSPacket %r' % self.header
+
+
+class Connection(object):
+    def __init__(self, sock):
+        self.sock = sock
+        self.ident = 'NBS1'
+
+    def __iter__(self):
+        self.sock.sendall(self.ident)
+        buf = ''
+        while True:
+            chunk = self.sock.recv(1024)
+            if chunk == '':
+                break
+            buf += chunk
+            if len(buf) < 12:
+                continue
+            
+            # Parse the packet envelope
+            data_id, data_size, data_checksum = struct.unpack('>III', buf[:12])
+            # data_id = 1 (full content)
+            # data_id = 2 (file name)
+            buf = buf[12:]
+
+            # We only support NBS1 (full content) feeds
+            if data_id != 1:
+                continue
+
+            while len(buf) < data_size:
+                buf += self.sock.recv(data_size)
+            packet = NBSPacket(buf[:data_size])
+            #if packet.checksum() != data_checksum:
+            #    print 'checksum mismatch!'
+            yield packet
+            buf = buf[data_size:]
+
+        log.error('Connection closed by remote host')
+        self.sock.close()
+
+
+class FileAssembler(object):
+    def __init__(self, filename, callback=None):
+        self.filename = filename
+        self.callback = callback
+        self.parts = {}
+
+    def add_part(self, packet):
+        self.parts[packet.header['block_number']] = packet.data
+        self.check_parts(packet)
+
+    def check_parts(self, packet):
+        if self.callback is None:
+            return
+
+        if not None in [self.parts.get(i, None) for i in range(1, packet.header['num_blocks'] + 1)]:
+            parts = self.parts.items()
+            parts.sort(key=lambda x: x[0])
+            content = ''.join([x[1] for x in parts])
+            self.content = content
+            self.callback(self.filename, self.content)
