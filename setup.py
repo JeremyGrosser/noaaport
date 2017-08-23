ab4aeb32fa650d2a0c6a57621f4b944a8363213e
Jeremy Grosser <jeremy@synack.me>
2017-08-22 21:14:52 -0700

    Package metadata

--- setup.py
+++ setup.py
@@ -1,6 +1,9 @@
 from setuptools import setup
 
 setup(name='noaaport',
+      version='1.1.0',
+      description='NOAAPORT and EMWIN weather data feed library',
+      url='https://github.com/JeremyGrosser/python-emwin',
       license='BSD-2-Clause',
       author='Jeremy Grosser',
       author_email='jeremy@synack.me',
